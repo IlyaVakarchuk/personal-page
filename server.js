78dca52a62fb0bdc27c8ee4c360e3dca245970b9
Ilya Vakarchuk <ilyavakarchuk@gmail.com>
2016-07-10 12:17:44 +0600

    bundle system for js

---
+++ server.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+
+var app = new (require('express'))();
+
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler));
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, function(error) {
+	if (error) {
+	console.error(error)
+	} else {
+
+	}
+})
