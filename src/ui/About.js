355128147f87c8d2f1910bdd03b6e3939e49e3e9
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-07-11 09:12:35 +0600

    code rafactoring

--- src/ui/About.js
+++ src/ui/About.js
@@ -1,37 +1,42 @@
 import React, { Component, PropTypes } from 'react';
 
 const About = class About extends Component {
+	constructor() {
+		super();
+
+		this.experience = [{
+			years : '2011 - 2015',
+			stage : 'Bachelor of Inpormation Technology',
+			place : 'SibSUTIS'
+		}, {
+			years : '2014 - 2014',
+			stage : 'QA - Enginee',
+			place : 'PinPay Express'
+		}, {
+			years : '2015 - 2015',
+			stage : 'Front-end Develop',
+			place : 'AcademMedia'
+		}, {
+			years : '2016',
+			stage : 'Web Developer',
+			place : 'NoveoGroup'
+		}]
+	}
+
 	render() {
 		return (
 			<div id='about-page'>
-				<div className='time-line'></div>
-				<div className='education-experience'>
-					<div className='experience-item'>
-						<div className='title'>2011 - 2015</div>
-						<div className='stage'>Bachelor of Inpormation Technology</div>
-						<div className='place'>SibSUTIS</div>
-					</div>
-				</div>
-				<div className='work-experience'>
-					<div className='experience-item'>
-						<div className='title'>2014 - 2014</div>
-						<div className='stage'>QA - Engineer</div>
-						<div className='place'>PinPay Express</div>
-					</div>
-				</div>
-				<div className='work-experience'>
-					<div className='experience-item'>
-						<div className='title'>2015 - 2015</div>
-						<div className='stage'>Web Developer</div>
-						<div className='place'>AcademMedia</div>
-					</div>
-				</div>
-				<div className='work-experience'>
-					<div className='experience-item'>
-						<div className='title'>2016</div>
-						<div className='stage'>Web Developer</div>
-						<div className='place'>NoveoGroup</div>
-					</div>
+				<div className='container'>
+					<div className='time-line'></div>
+					{this.experience.map(function(value, index) {
+						return <div key={index} className='experience'>
+							<div className='experience-item'>
+								<div className='title'>{value.years}</div>
+								<div className='stage'>{value.stage}</div>
+								<div className='place'>{value.place}</div>
+							</div>
+						</div>
+					})}
 				</div>
 			</div>
 		)
