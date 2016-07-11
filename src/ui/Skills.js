3aefd501b5ce2fdd95787f95cc91369d066f5d9d
Ilya Vakarchuk <ilya.vakarchuk@noveogroup.com>
2016-07-11 08:46:16 +0600

    change style for skills page

--- src/ui/Skills.js
+++ src/ui/Skills.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const Skills = class Skills extends Component {
 	constructor() {
 		super();
-		this.tech = {'javascript' : '67%', 'html/css' : '85%'};
+		this.tech = {'javascript' : '67%', 'html/css' : '85%', 'mongodb' : '71%', 'svg' : '33%'};
 	}
 
 	render() {
@@ -12,7 +12,7 @@ const Skills = class Skills extends Component {
 			<div id='skills-page'>
 				<div className='languages'>
 					<div className='container'>
-						<div className='title'>Skils</div>
+						<div className='title'>Skills</div>
 						<ul className='skills-list'>
 							{Object.keys(tech).map(function(value, index) {
 	              return <li className={'langitem ' + value} key={index}><span className='tech-title'>{value}</span><div style={{ width : tech[value]}} className='progress-bar'><span>{tech[value]}</span><div className='color-fill'></div></div></li>;
