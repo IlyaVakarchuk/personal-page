a2e82b552bc99f0ef35cd38d8a23ecbbec3bf815
Ilya Vakarchuk <ilyavakarchuk@gmail.com>
2016-07-19 14:31:15 +0600

    handlers for form values /

--- src/ui/Contact.js
+++ src/ui/Contact.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 const MailForm = class MailForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valueAuthorName : '',
+      valueAuthorEmail : '',
+      valueAuthorObject : '',
+      valueAuthorText : ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.sendMail = this.sendMail.bind(this);
+  }
+
+  handleChange (type, e) {
+    switch(type) {
+      case 'author-name' :
+        this.setState({valueAuthorName : e.target.value});
+        break;
+      case 'author-email' :
+        this.setState({valueAuthorEmail : e.target.value});
+        break;
+      case 'author-object' :
+        this.setState({valueAuthorObject : e.target.value});
+        break;
+      case 'author-text' :
+        this.setState({valueAuthorText : e.target.value});
+        break;
+      default:
+        break;
+    }
+  }
+
+  sendMail(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <form>
-        <input className='name' type='text' placeholder="Your's name" />
-        <input className='email' type='email' placeholder="Your's email" />
-        <input className='object' type='object' placeholder="Object of message" />
-        <textarea></textarea>
-        <input type='submit' value='Send' />
+        <input className='name' type='text' placeholder="Your's name" value={ this.state.valueAuthorName } onChange={ (event) => this.handleChange('author-name', event) } />
+        <input className='email' type='email' placeholder="Your's email" value={ this.state.valueAuthorEmail } onChange={ (event) => this.handleChange('author-email', event) } />
+        <input className='object' type='object' placeholder="Object of message" value={ this.state.valueAuthorObject } onChange={ (event) => this.handleChange('author-object', event) } />
+        <textarea value={ this.state.valueAuthorText } onChange={ (event) => this.handleChange('author-text', event) } ></textarea>
+        <input onClick={ this.sendMail } type='submit' value='Send' />
       </form>
     )
   }
